4baca7ff75aceae11c639849f3ccbdef31c6f08c
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-24 15:21:42 +0530

    Add project files.

---
+++ Myapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ReusableCodeLibrary;
+
+namespace Myapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.PrintString();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReusableCodeLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class Class1
+    {
+        public static void PrintString()
+        {
+            Console.WriteLine("Enter string");
+            string p;
+            p=Console.ReadLine();
+            Console.WriteLine(p);
+        }
+    }
+}
