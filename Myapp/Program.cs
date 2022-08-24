42a6b1029d4e032783cfab1236ad19b0255340a4
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-24 17:37:43 +0530

    commit

--- Myapp/Program.cs
+++ Myapp/Program.cs
@@ -6,7 +6,26 @@ namespace Myapp
     {
         static void Main(string[] args)
         {
-            Class1.PrintString();
+            string s, p;
+            Console.WriteLine("Enter the strings");
+           s= Console.ReadLine();
+            p= Console.ReadLine();
+            Class1.PrintString(s);
+            Class1.PrintString(s);
+            Class2.StringEquals(s, p);
+            string k;
+            Console.WriteLine("copied string");
+            Class3.Copy(s);
+            
+            Class4.index(s);
+            Console.WriteLine("Split");
+            string g = Console.ReadLine();
+            Class5.spled(g);
+            Class6.Upper();
+            Class7.create();
+            Class8.day();
+
+
             Console.ReadLine();
         }
     }
--- ReusableCodeLibrary/Class1.cs
+++ ReusableCodeLibrary/Class1.cs
@@ -3,17 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ReusableCodeLibrary
 {
+
     public class Class1
     {
-        public static void PrintString()
+        public static void PrintString(string s)
         {
-            Console.WriteLine("Enter string");
-            string p;
-            p=Console.ReadLine();
-            Console.WriteLine(p);
+            
+            Console.WriteLine(s);
         }
+    }
+    public class Class2
+    {
+        public static void StringEquals(string s,string p)
+        {
+            int k = s.Length;
+            int t = p.Length;
+            if (k == t)
+                Console.WriteLine("Strings are equal in length");
+            else
+                Console.WriteLine("Strings are not equal");
+
+        }
+    }
+    public class Class3
+    {
+        public static void Copy(string s)
+        {
+            string k;
+            k = s.Substring(0, s.Length);
+            Console.WriteLine(k);
+        }
+    }
+    public class Class4
+    {
+        public static void index(string s)
+        {
+            Console.WriteLine("Enter character");
+            string p=Console.ReadLine();
+            int k=s.IndexOf(p);
+            Console.WriteLine(k);
+
+        }
+    }
+    public class Class5
+    {
+        public static void spled(string g)
+        {
+            char[] sep = {'#'};
+            int cnt=1;
+            for(int i = 0; i < g.Length; i++)
+            {
+                if (g[i] == '#')
+                {
+                    cnt++;
+                }
+
+            }
+            Console.WriteLine(cnt);
+            string[] res = g.Split(sep,cnt,StringSplitOptions.RemoveEmptyEntries); 
+           for(int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+        }
+    }
+    public class Class6
+    {
+        public static void Upper()
+        {
+            string j=Console.ReadLine();
+            Console.WriteLine(j.ToUpper());
+
+        }
+
+    }
+    public class Class7
+    {
+        public static void create()
+        {
+            string s = @"C:\Users\saivignesh\source\repos\Myapp\firstfile.txt";
+            File.Create(s);
+
+        }
+
+    }
+    public class Class8
+    {
+        public static void day()
+        {
+            DateTime dt=DateTime.Now;
+            Console.WriteLine("Today's Properties");
+            Console.WriteLine(dt.Date);
+            Console.WriteLine(dt.Month);
+                Console.WriteLine(dt.Year);
+            Console.WriteLine(dt.Hour);
+            Console.WriteLine(dt.Minute);
+            Console.WriteLine(dt.Second);
+            Console.WriteLine(dt.Millisecond);
+
+        }
+
     }
 }
